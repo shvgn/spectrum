4c678b975de6135502a5ea93288d73be3f4d3c56
Evgeny Shevchenko <shvgn@protonmail.ch>
2016-04-23 01:38:50 +0300

    Renamed package to xy

--- analysis.go
+++ analysis.go
@@ -1,10 +1,10 @@
-// The code is provided "as is" without any warranty and shit.
+// The code is provided "as is" without any warranty and whatever.
 // You are free to copy, use and redistribute the code in any way you wish.
 //
 // Evgeny Shevchenko
 // shvgn@protonmail.ch
 // 2015
-package spectrum
+package xy
 
 import (
 	"log"
